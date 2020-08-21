f37eab2c810eb7d21f3e42050591fc57c2fc6bb8
Adwait Barkale <adwaitb@winjit.com>
2020-08-21 16:38:14 +0530

    Initial Commit

---
+++ SuperViews and SubViews/TableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TableViewCell.swift
+//  SuperViews and SubViews
+//
+//  Created by Adwait Barkale on 21/08/20.
+//  Copyright © 2020 Adwait Barkale. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var view: UIView!
+    @IBOutlet weak var lbl: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- SuperViews and SubViews/ViewController.swift
+++ SuperViews and SubViews/ViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var tblView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tblView.delegate = self
+        tblView.dataSource = self
+        //tblView.rowHeight = UITableView.automaticDimension
+        //tblView.estimatedRowHeight = 200
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tblView.dequeueReusableCell(withIdentifier: "TableViewCell") as! TableViewCell
+        //cell.view.superview?.backgroundColor = .blue
+        cell.view.superview?.superview?.backgroundColor = .blue //cell
+        cell.view.superview?.superview?.layer.borderWidth = 4
+        cell.view.superview?.superview?.layer.borderColor = UIColor.orange.cgColor
+        let items = cell.subviews // Cell SubView -> Content View
+        let v = items[0].subviews //ContentView SubView -> UIView
+        v[0].backgroundColor = .green //Change UIView color
+        print(v)
+        let l = v[0].subviews
+        print(l)
+        l[0].backgroundColor = .yellow
+        print(items.count)
+        _ = items[1]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
 }
 
+//    func changeWithoutOutlets()
+//    {
+//        myView.superview?.backgroundColor = .green
+//        if let textfields = myView.subviews as? [UITextField]{
+//            textfields[0].text = "Adwait"
+//            textfields[1].text = "barkale"
+//            textfields[0].superview?.backgroundColor = .red
+//        }
+//    }
+    
